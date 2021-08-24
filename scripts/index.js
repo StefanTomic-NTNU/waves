d4500cfc331937f0b949d2741c90ce6947eb4bd7
Stefan Djordje Tomic <stefandt@stud.ntnu.no>
2021-08-24 18:23:48 +0200

    Better res, boat

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,14 @@
 let x_offset = 0;
 let canvas;
 let ctx;
+const scale = 10;
 
 $(document).ready(function () {
 
     canvas = $("#canvas1")[0];
     ctx = canvas.getContext("2d");
 
-    // https://stackoverflow.com/questions/29917446/drawing-sine-wave-in-canvas
-
-    loop_waves(100);
+    runAnimation(50);
 
     $("#gallery_title").hover(function () {
             $(this).css("color", "red");
@@ -23,16 +22,26 @@ $(document).ready(function () {
 })
 
 
-function draw_waves() {
-    const l = 60; // length
-    const heightRatio = 0.6;
-    const tf = l / 5; // two fifths of length
-
-    // ctx.save();
+function drawCanvas() {
+    // https://stackoverflow.com/questions/29917446/drawing-sine-wave-in-canvas
 
     ctx.clearRect(-canvas.width, -canvas.height, 2*canvas.width, 2*canvas.height); // clear the canvas
-     // just for display
     ctx.save();
+
+    $('#canvas1').css('background-color', 'azure');
+    drawWaves();
+    drawBoat();
+
+
+    ctx.restore();
+}
+
+
+
+function drawWaves() {
+    const l = 80; // wavelength
+    const heightRatio = 0.55;
+    const tf = l * 3 / 10; // two fifths of length for sine
 
     if (x_offset >= l) {
         x_offset = 0
@@ -41,28 +50,43 @@ function draw_waves() {
     }
     ctx.translate(-x_offset, 0);
 
+    ctx.strokeStyle = "darkblue";
     ctx.beginPath();
-    ctx.strokeStyle ='darkblue';
 
     ctx.moveTo(0,0);
-    for(let i = 0; i <= 10 ; i++) {
+    for(let i = 0; i <= 15 ; i++) {
         ctx.bezierCurveTo(tf,-(l*heightRatio-tf),l-tf,l*heightRatio-tf,l,0);
         ctx.translate(l, 0);
     }
 
-    //ctx.moveTo(canvas.width, 100);
     ctx.lineTo(canvas.width, canvas.height);
     ctx.lineTo(-3*canvas.width, canvas.height);
     ctx.closePath();
     ctx.fillStyle = "darkblue";
     ctx.fill();
     ctx.stroke();
-
-    ctx.restore();
-
 }
 
-function loop_waves(speed) {
-    ctx.translate(0, 100);
-    setInterval(draw_waves, speed);
+
+function drawBoat() {
+    ctx.restore();
+    ctx.translate(canvas.width/2, 0);
+    ctx.beginPath();
+    ctx.moveTo(-50,10)
+    ctx.strokeStyle = "lightgrey";
+    ctx.lineTo(50, 10);
+    ctx.lineTo(70, -10);
+    ctx.lineTo(-50, -10);
+    ctx.closePath();
+    ctx.fillStyle = "white";
+    ctx.fill();
+    ctx.stroke();
+    ctx.translate(-canvas.width/2, 0);
+    ctx.strokeStyle = "darkblue";
+}
+
+
+function runAnimation(speed) {
+    ctx.translate(0, canvas.height / 2);
+    setInterval(drawCanvas, speed);
 }
